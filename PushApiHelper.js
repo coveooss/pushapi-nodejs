ac3bdf9b48d707c197833728506923856ac4aa3b
JDevost <>
2019-01-02 11:59:51 -0500

    v0.0.6 support case insensitive keys like AddOrUpdate and addorupdate

--- PushApiHelper.js
+++ PushApiHelper.js
@@ -8,8 +8,7 @@ class PushApiHelper {
     this._dir = process.cwd();
     try {
       this.config = require(`${this._dir}/.pushapi-config`);
-    }
-    catch (e) {
+    } catch (e) {
       PushApiHelper.throwError(`Couldn't load .pushapi-config.json file from ${this._dir}`);
     }
 
@@ -17,35 +16,44 @@ class PushApiHelper {
     this._debug('\nconfig = ', this.config);
   }
 
-  _debug () {
+  _debug() {
     if (this.config.debug) {
       console.debug.apply(console, arguments);
     }
   }
 
-  _log () {
+  _log() {
     console.log.apply(console, arguments);
   }
 
-  _sendRequest (method, action) {
+  /**
+   * Utility function to check if a key in a JSON object. The JSON payload for the Push Api is case-insensitive, so DocumentId is the same as documentid.
+   * @param {string} key
+   * @param {Object} obj
+   */
+  _isKeyMissingInObject(key, obj) {
+    let keys = Object.keys(obj).map(k => k.toLowerCase());
+    return !(keys.includes(key.toLowerCase()));
+  }
+
+  _sendRequest(method, action) {
     let config = this.config,
       url = /^http/.test(action) ? action : `https://${config.platform}/v1/organizations/${config.org}/sources/${config.source}/${action}`;
 
     return new Promise((resolve, reject) => {
       request({
-        method: method,
-        url: url,
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + config.apiKey
-        }
-      },
+          method: method,
+          url: url,
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + config.apiKey
+          }
+        },
         (err, httpResponse, body) => {
           this._debug('\nREQUEST: ', method, url, httpResponse && httpResponse.statusCode, err);
           if (!err && [200, 201, 202].includes(httpResponse.statusCode)) {
             resolve(body);
-          }
-          else {
+          } else {
             console.log('ERROR: ', err, httpResponse.statusCode, url);
             reject(err || httpResponse.statusCode);
           }
@@ -53,11 +61,11 @@ class PushApiHelper {
     });
   }
 
-  changeStatus (state) {
+  changeStatus(state) {
     return this._sendRequest(`POST`, `status?statusType=${state}`);
   }
 
-  getLargeFileContainer () {
+  getLargeFileContainer() {
     let config = this.config;
     return this._sendRequest(`POST`, `https://${config.platform}/v1/organizations/${config.org}/files`).then(
       body => {
@@ -74,28 +82,29 @@ class PushApiHelper {
     );
   }
 
-  sendBatchRequest (fileId) {
+  sendBatchRequest(fileId) {
     return this._sendRequest(`PUT`, `documents/batch?fileId=${fileId || this.fileId}`);
   }
 
-  uploadBatchFile (data) {
+  uploadBatchFile(data) {
     return new Promise((resolve, reject) => {
 
       request.put({
-        url: this.uploadUri,
-        headers: {
-          'Content-Type': 'application/octet-stream',
-          'x-amz-server-side-encryption': 'AES256'
+          url: this.uploadUri,
+          headers: {
+            'Content-Type': 'application/octet-stream',
+            'x-amz-server-side-encryption': 'AES256'
+          },
+          body: JSON.stringify(data)
         },
-        body: JSON.stringify(data)
-      },
         (err, httpResponse, body) => {
           if (!err && [200, 201, 202].includes(httpResponse.statusCode)) {
-            this._debug('Batch file sent to AWS. ', new Date().toLocaleTimeString('en-US', { hour12: false }));
+            this._debug('Batch file sent to AWS. ', new Date().toLocaleTimeString('en-US', {
+              hour12: false
+            }));
             this._log(body);
             resolve(body);
-          }
-          else {
+          } else {
             console.log('ERROR: ', err, httpResponse.statusCode, this.uploadUri);
             reject(err);
           }
@@ -103,15 +112,18 @@ class PushApiHelper {
     });
   }
 
-  pushFile (data) {
+  pushFile(data) {
     // validate payload first
     if (!data) {
       console.warn('Invalid payload: not defined.');
     }
     if (data instanceof Array) {
-      data = { AddOrUpdate: data }; // need to wrap arrays of documents into AddOrUpdate
+      data = {
+        AddOrUpdate: data
+      }; // need to wrap arrays of documents into AddOrUpdate
     }
-    if (!data.AddOrUpdate) {
+
+    if (this._isKeyMissingInObject('AddOrUpdate', data)) {
       // wrap payload into {"AddOrUpdate": [data]}
       data = {
         AddOrUpdate: [
@@ -119,15 +131,26 @@ class PushApiHelper {
         ]
       };
     }
+    else if (!data.AddOrUpdate) {
+      // AddOrUpdate is present, but using a different case.
+
+      // find the key using the different case
+      let key = Object.keys(data).filter(k=>k.match(/AddOrUpdate/i))[0];
+
+      // replacing key by 'AddOrUpdate'
+      data.AddOrUpdate = data[key];
+      delete data[key];
+    }
+
+
     let fileExtensionWarning = false;
     // validate each document has a DocumentId
     data.AddOrUpdate.forEach(d => {
-      let keys = Object.keys(d).map(k => k.toLowerCase());
-      if (!keys.includes('documentid')) {
+      if (this._isKeyMissingInObject('DocumentId', d)) {
         console.warn(`Missing DocumentId in some documents in the payload. Stopping.`);
         throw new Error('No DocumentId.');
       }
-      if (!fileExtensionWarning && !keys.includes('fileextension')) {
+      if (!fileExtensionWarning && this._isKeyMissingInObject('FileExtension', d)) {
         console.log(`Missing FileExtension in some documents. It's good practice to provide them.`);
         fileExtensionWarning = true;
       }
@@ -144,12 +167,12 @@ class PushApiHelper {
       });
   }
 
-  static throwError (msg, code) {
+  static throwError(msg, code) {
     console.warn(`\n\t${msg}`);
     process.exit(code || 1);
   }
 
-  validateConfig () {
+  validateConfig() {
     if (!this.config) {
       PushApiHelper.throwError('Missing config (.pushapi-config.json)', 2);
     }
